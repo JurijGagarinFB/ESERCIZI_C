52110c057e5588b60915c739512688474ac55239
JurijGagarinFB <150333738+JurijGagarinFB@users.noreply.github.com>
2024-10-01 07:46:57 +0200

    Create Es_8.c

---
+++ Es_8.c
@@ -0,0 +1,31 @@
+//Bazaj Francesco 4F
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n, numero, i = 0;
+    double somma = 0;
+    do
+    {
+        printf("Inserisci N maggiore di zero: ");
+        scanf("%d", &n);
+    } while (n <= 0);
+    do
+    {
+        printf("Inserisci un numero: \n");
+        scanf("%d", &numero);
+        i++;
+        if (numero >= 0)
+        {
+            somma = somma + sqrt(numero);
+        }
+        else
+        {
+            printf("Hai inserito un numero negativo\n");
+        }
+    } while (i < n && numero >= 0);
+    printf("La somma è: %f", somma);
+    return 0;
+}
