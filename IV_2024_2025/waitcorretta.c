0bc5d1a716913a4105ab0bd543c65f56fcbc6e21
JurijGagarinFB <francesco.bazaj@iisviolamarchesini.edu.it>
2025-01-16 09:05:32 +0100

    update repo

--- IV_2024_2025/waitcorretta.c
+++ IV_2024_2025/waitcorretta.c
@@ -1,24 +1,40 @@
+/**
+ * @brief Esempio utilizzo funzione "wait()"" con corretto uso dell'argomento "status"
+ *
+ * @param argc numero di parametri passati da linea di comando
+ * @param argv array di stringhe che contiene i parametri passati da linea di comando
+ *
+ * @return 0 il programma ritorna 0
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include <unistd.h>   //serve per la fork()
+#include <sys/wait.h> //serve per la wait()
 
 int p;
 int status;
 
-int main()
+int main(int argc, char *argv[])
 {
     p = fork();
 
-    if (p == 0) //figlio
+    if (p == 0) // figlio
     {
-
+        printf("1. Sono il figlio PID=%d \n", getpid());
+        printf("2. Sono il figlio PID=%d mio padre ha PID=%d \n", getpid(), getppid());
+        exit(255);
     }
-    else if (p > 0) //padre
+    else if (p > 0) // padre
     {
+        printf("4. Sono il padre PID=%d \n", getpid());
+        printf("5. Sono il padre PID=%d mio figlio ha PID=%d \n", getpid(), p);
+        printf("Padre %d attendo la terminazione di mio figlio %d \n", getpid(), wait(&status));
+        printf("Mio figlio mi ha passato il valore %d \n", WEXITSTATUS(status));
     }
-    else //errore
+    else // errore
     {
+        printf("Errore fork \n");
     }
     return 0;
 }
