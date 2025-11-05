800887249d25d6abebee1b2a48b0dad9cd54c731
JurijGagarinFB <francesco.bazaj@iisviolamarchesini.edu.it>
2025-11-05 12:29:50 +0100

    socket esercizi

---
+++ V_2025_2026/socket/ES_socket_2/Server2.c
@@ -0,0 +1,67 @@
+/*
+Esercizio 2
+Scrivere il codice in C, di un applicazione Socket CLIENT-SERVER in cui il server riceve in input 1 stringa
+e un carattere, e dopo aver effettuato gli eventuali ed opportuni controlli (se necessari), rispedisce al Client il
+numero di occorrenze del carattere nella stringa.
+*/
+
+// SERVER
+#include <stdio.h>      //std in-out
+#include <stdlib.h>     //per utilizzo di certe funzioni:htonl,rand,....
+#include <sys/socket.h> //funz. accept+bind+listen
+#include <sys/types.h>  //funz. accept
+#include <netinet/in.h> //definiscono la struttura degli indirizzi
+#include <netdb.h>      //strutture hostent e servent che identificano l'host tramite iol nome
+#include <string.h>     //funz. stringhe
+#include <fcntl.h>      //descrittore di file
+#include <signal.h>     //consente l'utilizzo delle funzioni per la gestione dei segnali fra processi
+#include <errno.h>      //gestioni errori connessione
+#include <ctype.h>      //bind
+#include <unistd.h>     // file header che consente l'accesso alle API dello standard POSIX
+
+#define DIM 50
+#define SERVERPORT 1450
+
+int trovaOccorrenze(char str[], char charDaCercare){
+    int count = 0;
+    for(int i = 0; i < strlen(str); i++){
+        if(str[i] == charDaCercare){
+            count++;
+        }
+    }
+    return count;
+}
+
+int main(){
+    struct sockaddr_in servizio, addr_remoto; // record con i dati del server e del client
+
+    servizio.sin_family = AF_INET;
+    servizio.sin_addr.s_addr = htonl(INADDR_ANY);
+    servizio.sin_port = htons(SERVERPORT);
+    int socketfd, soa, fromlen = sizeof(servizio);
+    char str[DIM], charDaCercare, response[DIM];
+
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    bind(socketfd, (struct sockaddr *)&servizio, sizeof(servizio));
+    listen(socketfd, 10);
+
+    for(;;)
+    {
+        printf("\n\nServer in ascolto...");
+        fflush(stdout);
+
+        soa = accept(socketfd, (struct sockaddr *)&addr_remoto, &fromlen);
+        read(soa, str, sizeof(str));
+        printf("Stringa ricevuta: %s\n", str);
+        
+        read(soa, &charDaCercare, sizeof(charDaCercare));
+        printf("Carattere da cercare ricevuto: %c\n", charDaCercare);
+
+        sprintf(response, "Il carattere '%c' appare %d volte nella stringa.", charDaCercare, trovaOccorrenze(str, charDaCercare));
+
+        printf("Invio risposta al client... %s\n", response);
+        write(soa, response, sizeof(response));
+        close(soa);
+    }
+    return 0;
+}
